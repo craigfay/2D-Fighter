d2a1ded1dfebc838a7a62fe087d664967c6ba9da
craig fay <craigfay1@gmail.com>
2019-12-05 00:18:45 -0600

    Use gdnative as a submodule

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,27 @@
-use gdnative::*;
+#[macro_use]
+extern crate gdnative;
 
-/// The HelloWorld "class"
-#[derive(NativeClass)]
-#[inherit(Node)]
-pub struct HelloWorld;
+#[derive(gdnative::NativeClass)]
+#[inherit(gdnative::Node)]
+#[user_data(gdnative::user_data::ArcData<HelloWorld>)]
+struct HelloWorld;
 
-// __One__ `impl` block can have the `#[methods]` attribute, which will generate
-// code to automatically bind any exported methods to Godot.
-#[methods]
+#[gdnative::methods]
 impl HelloWorld {
-    
-    /// The "constructor" of the class.
-    fn _init(_owner: Node) -> Self {
+    fn _init(_owner: gdnative::Node) -> Self {
         HelloWorld
     }
-    
-    // In order to make a method known to Godot, the #[export] attribute has to be used.
-    // In Godot script-classes do not actually inherit the parent class.
-    // Instead they are "attached" to the parent object, called the "owner".
-    // The owner is passed to every single exposed method.
+
     #[export]
-    fn _ready(&self, _owner: Node) {
-        // The `godot_print!` macro works like `println!` but prints to the Godot-editor
-        // output tab as well.
-        godot_print!("hello, world.");
+    fn _ready(&self, _owner: gdnative::Node) {
+        godot_print!("hello, world.")
     }
 }
 
-// Function that registers all exposed classes to Godot
 fn init(handle: gdnative::init::InitHandle) {
     handle.add_class::<HelloWorld>();
 }
 
-// macros that create the entry-points of the dynamic library.
 godot_gdnative_init!();
 godot_nativescript_init!(init);
 godot_gdnative_terminate!();
