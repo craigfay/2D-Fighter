67dda42b36b0ebd8fb551c2cc711ec887a20fcfa
craig fay <craigfay1@gmail.com>
2019-12-04 23:28:15 -0600

    introduce lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,38 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use gdnative::*;
+
+/// The HelloWorld "class"
+#[derive(NativeClass)]
+#[inherit(Node)]
+pub struct HelloWorld;
+
+// __One__ `impl` block can have the `#[methods]` attribute, which will generate
+// code to automatically bind any exported methods to Godot.
+#[methods]
+impl HelloWorld {
+    
+    /// The "constructor" of the class.
+    fn _init(_owner: Node) -> Self {
+        HelloWorld
+    }
+    
+    // In order to make a method known to Godot, the #[export] attribute has to be used.
+    // In Godot script-classes do not actually inherit the parent class.
+    // Instead they are "attached" to the parent object, called the "owner".
+    // The owner is passed to every single exposed method.
+    #[export]
+    fn _ready(&self, _owner: Node) {
+        // The `godot_print!` macro works like `println!` but prints to the Godot-editor
+        // output tab as well.
+        godot_print!("hello, world.");
     }
 }
+
+// Function that registers all exposed classes to Godot
+fn init(handle: gdnative::init::InitHandle) {
+    handle.add_class::<HelloWorld>();
+}
+
+// macros that create the entry-points of the dynamic library.
+godot_gdnative_init!();
+godot_nativescript_init!(init);
+godot_gdnative_terminate!();
