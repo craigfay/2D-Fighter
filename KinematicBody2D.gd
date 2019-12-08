6fecb5c97e79967ca6f5b5f1281dc4ef67d4e7c5
craig fay <craigfay1@gmail.com>
2019-12-07 23:39:31 -0600

    Add gravity/collisions

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -3,6 +3,10 @@ extends KinematicBody2D
 var motion = Vector2()
 
 func _physics_process(delta):
+    # Gravity
+    motion.y += 10
+
+    # Left/Right motion
     if Input.is_action_pressed("ui_left"):
         motion.x = -100
     elif Input.is_action_pressed("ui_right"):
