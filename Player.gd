ac6304ad4d86e0ff862e29268ed18f2d17855e92
craig fay <craigfay1@gmail.com>
2019-12-08 00:00:25 -0600

    Add jump physics

--- Player.gd
+++ Player.gd
@@ -1,17 +1,21 @@
 extends KinematicBody2D
 
+const UP = Vector2(0, -1)
 var motion = Vector2()
 
 func _physics_process(delta):
     # Gravity
-    motion.y += 8
+    motion.y += 16
+    motion = move_and_slide(motion, UP)
 
     # Left/Right motion
     if Input.is_action_pressed("ui_left"):
-        motion.x = -64
+        motion.x = -96
     elif Input.is_action_pressed("ui_right"):
-        motion.x = 64
+        motion.x = 96
     else:
         motion.x = 0
 
-    move_and_slide(motion)
+    if is_on_floor():
+        if Input.is_action_just_pressed("ui_up"):
+            motion.y = -320
