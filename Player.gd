14ec15509704b6be36deae60ea111ee22e252461
craig fay <craigfay1@gmail.com>
2019-12-08 00:05:37 -0600

    Use physics variables

--- Player.gd
+++ Player.gd
@@ -3,19 +3,24 @@ extends KinematicBody2D
 const UP = Vector2(0, -1)
 var motion = Vector2()
 
+var walk_speed = 96
+var gravity_force = 16
+var jump_force = 320
+
 func _physics_process(delta):
     # Gravity
-    motion.y += 16
+    motion.y += gravity_force
     motion = move_and_slide(motion, UP)
 
     # Left/Right motion
     if Input.is_action_pressed("ui_left"):
-        motion.x = -96
+        motion.x = -walk_speed
     elif Input.is_action_pressed("ui_right"):
-        motion.x = 96
+        motion.x = walk_speed
     else:
         motion.x = 0
 
+    # Jump
     if is_on_floor():
         if Input.is_action_just_pressed("ui_up"):
-            motion.y = -320
+            motion.y = -jump_force
