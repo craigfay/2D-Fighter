faf9268004030d620f683ba32baa8d3432a82516
craig fay <craigfay1@gmail.com>
2019-12-07 23:47:23 -0600

    Group multiple walls and tweak physics

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -4,13 +4,13 @@ var motion = Vector2()
 
 func _physics_process(delta):
     # Gravity
-    motion.y += 10
+    motion.y += 8
 
     # Left/Right motion
     if Input.is_action_pressed("ui_left"):
-        motion.x = -100
+        motion.x = -64
     elif Input.is_action_pressed("ui_right"):
-        motion.x = 100
+        motion.x = 64
     else:
         motion.x = 0
 
