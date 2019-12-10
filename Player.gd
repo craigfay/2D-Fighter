68cb6ab3a32ab46e7dd37c8ce35e2108facdb864
craig fay <craigfay1@gmail.com>
2019-12-09 22:31:18 -0600

    WIP introduce sprite animation

--- Player.gd
+++ Player.gd
@@ -10,6 +10,13 @@ export var walk_speed = 96
 export var gravity_force = 32
 export var jump_force = 640
 
+# Animations
+var current_animation = {
+    "frames": IDLE_ANIMATION,
+    "repeats": true,
+    "index": 0
+}
+
 func _process(delta):
     # Toggle off the previous state
     for child in $States.get_children():
@@ -20,12 +27,17 @@ func _process(delta):
         if is_actionable:
             if is_on_floor():
                 $States.get_node("CROUCH_0").visible = true
-                return
 
     # Idle
     if is_actionable:
         $States.get_node("IDLE_0").visible = true
-        return
+
+    # Advance animation frame
+    $Sprite.set('frame', current_animation.frames[current_animation.index])
+    if current_animation.index == (current_animation.frames.size() - 1):
+        current_animation.index = 0
+    else:
+        current_animation.index += 1
 
 
 func _physics_process(delta):
@@ -47,3 +59,16 @@ func _physics_process(delta):
     if is_on_floor():
         if Input.is_action_just_pressed("ui_up"):
             motion.y = -jump_force
+
+const IDLE_ANIMATION = [
+    10,10,10,10,
+    11,11,11,11,
+    12,12,12,12,
+    13,13,13,13,
+    14,14,14,14,
+    15,15,15,15,
+    16,16,16,16,
+    17,17,17,17,
+    18,18,18,18,
+    19,19,19,19,
+]
